626578e9e9a82a8bd140a16186b509461f17df28
SyAnh1703 <dosyanh1703@gmail.com>
2022-04-23 14:00:15 +0700

    first commit

--- data_collect.py
+++ data_collect.py
@@ -11,7 +11,7 @@ from firebase_admin import db
 from datetime import datetime
 
 #Flask
-from flask import Flask, redirect, render_template, url_for, jsonify
+from flask import Flask, redirect, render_template, jsonify
 from sqlalchemy import true
 
 sys.path.insert(0, "..")
@@ -23,29 +23,32 @@ firebase_admin.initialize_app(cred,{
 })
 
 #Put into the web app
-app = Flask(__name__)
+app = Flask(__name__)  
 
 @app.route('/data_json')
 def data_json():
+    
     time_display = datetime.now()
     time_str = time_display.strftime("%H:%M")
     date_str = time_display.strftime("%d-%m-%Y")
 
     # Get data from Firebase
-    ref_pH = db.reference('/Sensor/pH/' + str(date_str) + '/value')
-    ref_DO = db.reference('/Sensor/DO/' + str(date_str) + '/value')
-    ref_Temp= db.reference('/Sensor/Temperature/' + str(date_str) + '/value')
+    ref = db.reference('Sensor')
+    pH_ref = ref.child('pH').child(str(date_str)).child('value') #Get the key named pH
+    DO_ref = ref.child('DO').child(str(date_str)).child('value') #Get the key named DO
+    Temp_ref = ref.child('Temperature').child(str(date_str)).child('value') #Get the key named Temperature
 
-    #Convert to float 
-    float_pH = float(ref_pH.get())
-    float_DO = float(ref_DO.get())
-    float_Temp = float(ref_Temp.get())    
-   
+    #Convert to string 
+    str_pH = str(pH_ref.get())
+    str_DO = str(DO_ref.get())
+    str_Temp = str(Temp_ref.get()) 
+
+    #Data set for the present value
     sensor_data=[
         {
             "id": 0,
             "name": "DO",
-            "value": float_DO,
+            "value": str_DO,
             "date": date_str,
             "time": time_str
         },
@@ -53,28 +56,66 @@ def data_json():
         {
             "id": 1,
             "name": "Temperature",
-            "value": float_Temp,
+            "value": str_Temp,
             "date": date_str,
             "time": time_str
         },
 
         {
-            "id" :2,
+            "id" :2,    
             "name": "pH",
-            "value": float_pH,
+            "value": str_pH,
             "date": date_str,
             "time": time_str
         }
     ]
-    return jsonify(sensor_data)
+    return jsonify(sensor_data)  
+
+@app.route('/hourly_data_json')
+def hourly_data_json():
+    
+    time_display = datetime.now()
+
+    date_str = time_display.strftime("%d-%m-%Y")
+
+    # Get data from Firebase
+    ref = db.reference('Sensor')
+    pH_ref = ref.child('pH').child(str(date_str)).child('value') #Get the key named pH
+    DO_ref = ref.child('DO').child(str(date_str)).child('value') #Get the key named DO
+    Temp_ref = ref.child('Temperature').child(str(date_str)).child('value') #Get the key named Temperature
+
+    #Convert to string 
+    str_pH = str(pH_ref.get())
+    str_DO = str(DO_ref.get())
+    str_Temp = str(Temp_ref.get())  
+
+    #hourly_value = str("value at "+ str(x) + ":00")
+    hourly_set = [
+        {
+            "id": 3,
+            "hour": 0,
+            "value": str_DO
+        },
+        {
+            "id": 4,
+            "hour": 0,
+            "value":str_Temp
+        },
+        {
+            "id": 5,
+            "hour": 0,
+            "value":str_pH
+        }
+    ]
+    return jsonify(hourly_set)  
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     #Get jsonified data
     sensor_data = data_json()
+    hourly_set = hourly_data_json()
     return render_template('sensors.html', 
-    data=sensor_data.json)
-
+    data=sensor_data.json, hourly_array=hourly_set.json)
 
 if __name__=="__main__":
     app.run(debug=True)
