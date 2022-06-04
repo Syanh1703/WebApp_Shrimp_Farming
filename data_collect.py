173cfe16a573bda678c503719a87b19047734749
SyAnh1703 <dosyanh1703@gmail.com>
2022-06-04 16:51:07 +0700

    Time performance

--- data_collect.py
+++ data_collect.py
@@ -1,132 +1,41 @@
-
-from dataclasses import dataclass
 import sys
-from time import time
-from unicodedata import name
+from time import perf_counter
 
-#Firebase Admin
+# Firebase Admin
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from datetime import datetime
 
-#Flask
-from flask import Flask, redirect, render_template, jsonify
-from sqlalchemy import true
+# Flask
+from flask import Flask, render_template, jsonify
 
 sys.path.insert(0, "..")
 
-
 cred = credentials.Certificate('app.json')
-firebase_admin.initialize_app(cred,{
+firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://thesissensordatabase-de170-default-rtdb.firebaseio.com'
 })
 
-#Put into the web app
-app = Flask(__name__)  
+class MyNode():
 
-@app.route('/data_json')
-def data_json():
-    
-    time_display = datetime.now()
-    time_str = time_display.strftime("%H:%M")
-    date_str = time_display.strftime("%d-%m-%Y")
+    #a dict to store data
+    def __init__(self, sensorName):
+        self.sensorName = sensorName
+        self.year = 0
+        self.date = 0
+        self.hour = 0
+        self.minute = 0
+        self.avg = 0.0
+        self.pH_array_hourly = []
+        self.DO_array_hourly = []
+        self.Temp_array_hourly = []
+        self.pH_array_daily = []
+        self.DO_array_daily = []
+        self.Temp_array_daily = []
 
-    # Get data from Firebase
-    ref = db.reference('Sensor')
-    pH_ref = ref.child('pH').child(str(date_str)).child('value') #Get the key named pH
-    DO_ref = ref.child('DO').child(str(date_str)).child('value') #Get the key named DO
-    Temp_ref = ref.child('Temperature').child(str(date_str)).child('value') #Get the key named Temperature
+    def call_api(self):
+        pass
 
-    #Convert to string 
-    str_pH = str(pH_ref.get())
-    str_DO = str(DO_ref.get())
-    str_Temp = str(Temp_ref.get()) 
-
-    #Data set for the present value
-    sensor_data=[
-        {
-            "id": 0,
-            "name": "DO",
-            "value": str_DO,
-            "date": date_str,
-            "time": time_str
-        },
-
-        {
-            "id": 1,
-            "name": "Temperature",
-            "value": str_Temp,
-            "date": date_str,
-            "time": time_str
-        },
-
-        {
-            "id" :2,    
-            "name": "pH",
-            "value": str_pH,
-            "date": date_str,
-            "time": time_str
-        }
-    ]
-    return jsonify(sensor_data)  
-
-@app.route('/hourly_data_json')
-def hourly_data_json():
-    
-    time_display = datetime.now()
-
-    date_str = time_display.strftime("%d-%m-%Y")
-
-    # Get data from Firebase
-    ref = db.reference('Sensor')
-    pH_ref = ref.child('pH').child(str(date_str)).child('value') #Get the key named pH
-    DO_ref = ref.child('DO').child(str(date_str)).child('value') #Get the key named DO
-    Temp_ref = ref.child('Temperature').child(str(date_str)).child('value') #Get the key named Temperature
-
-    #Convert to string 
-    str_pH = str(pH_ref.get())
-    str_DO = str(DO_ref.get())
-    str_Temp = str(Temp_ref.get())  
-
-    #hourly_value = str("value at "+ str(x) + ":00")
-    hourly_set = [
-        {
-            "id": 3,
-            "hour": 0,
-            "value": str_DO
-        },
-        {
-            "id": 4,
-            "hour": 0,
-            "value":str_Temp
-        },
-        {
-            "id": 5,
-            "hour": 0,
-            "value":str_pH
-        }
-    ]
-    return jsonify(hourly_set)  
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    #Get jsonified data
-    sensor_data = data_json()
-    hourly_set = hourly_data_json()
-    return render_template('sensors.html', 
-    data=sensor_data.json, hourly_array=hourly_set.json)
-
-if __name__=="__main__":
-    app.run(debug=True)
-    def minute_interval(min_before, min_after):
-        if (true):
-            min_before = 0
-            min_after = 0
-            print("min_before is:" + str(min_before))
-            print("min_after is: " + str(min_after))
-            index()
-        else:
-            print("Goodbye")
-
-      
+if __name__ == "__main__":
+    print("hello")
--- test.py
+++ test.py
@@ -4,11 +4,8 @@ import sys
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from datetime import date, datetime
+from datetime import datetime
 import time
-
-#Rasp Library
-
 
 sys.path.insert(0, "..")
 
@@ -19,88 +16,50 @@ firebase_admin.initialize_app(cred, {
     
 time_display = datetime.now()
 time_str = time_display.strftime("%H:%M")
-date_str = time_display.strftime("%d-%m-%Y")
+date_str = time_display.strftime("%Y-%d-%m")
+date_display = str(date_str + "_" + time_str)
+ref = db.reference("/Node1")
+
+pH_ref = ref.child(str(date_display)).child("PH") #Get the key named pH
+DO_ref = ref.child(str(date_display)).child("DO") #Get the key named DO
+Temp_ref = ref.child(str(date_display)).child("T") #Get the key named Temperature
 
 if __name__ == "__main__":
-
-    #Set up the data set first
-    ref = db.reference("/")
-    ref.set({
-        'Sensor':
-            {
-                'DO':
-                    {
-                        date_str:
-                        {
-                            'value': '35'
-                        }
-                    },
-                'pH':
-                    {
-                        date_str:
-                        {
-                            'value': '10'
-                        }
-                    },
-                'Temperature':
-                    {
-                        date_str:
-                        {
-                            'value': '3'
-                        }
-                    }
-            }
-    })
-
     # Put into time interval
     def minute_interval(min_before, min_after):
-        if (min_after - min_before) >= 1:
+        if((min_after - min_before)>0): #Update per 5 min
+            time_display = datetime.now()
+            year_str = time_display.year
+            time_str = time_display.strftime("%H:%M")
+            date_str = time_display.strftime("%d-%m")
+            ref_time = ref.child(str(year_str)).child(str(date_str))
+            #Update data
+            ref_time.update({
+                str(time_str):
+                {
+                    "PH":"9",
+                    "DO":"12",
+                    "T": "34"
+                }
 
-            #Trial update the value
-            ref = db.reference('Sensor')
-            pH_ref = ref.child('pH').child(str(date_str)) #Get the key named pH
-            DO_ref = ref.child('DO').child(str(date_str)) #Get the key named DO
-            Temp_ref = ref.child('Temperature').child(str(date_str)) #Get the key named Temperature
-
-            #Cretae a for loop for continuous update within 24 Hours
-            for x in range(25):
-                #Temperature
-                Temp_value_index = str("value at " + str(x) + ":00")
-                Temp_ref_dummy_value = 50 + int(x)
-                Temp_ref.update({
-                    str(Temp_value_index): Temp_ref_dummy_value
-                })
-
-                #DO
-                DO_ref_value_index = str("value at " + str(x) + ":00")
-                DO_ref_dummy_value = 60 + int(x)
-                DO_ref.update({
-                    str(DO_ref_value_index): DO_ref_dummy_value
-                })
-
-                #pH
-                pH_ref_value_index = str("value at "+ str(x) + ":00")
-                pH_ref_dummy_value = 80 + int(x)
-                pH_ref.update({
-                    str(pH_ref_value_index):pH_ref_dummy_value
-                })
-            #Restart the minute
-            min_before = 0
-            min_after = 0
-            print("min_before is:" + str(min_before))
-            print("min_after is: " + str(min_after))
+            })
+            print("Complete the update")
 
         else:
             print("Goodbye")
-
-
-    while 1:
+            
+    while (True):
         min1 = datetime.now().minute
         print("min1 is :" + str(min1))
 
-        time.sleep(60)  # Pause the process by 1 minute
+        time.sleep(60)  # Pause the process by 5 minute
 
         min2 = datetime.now().minute
-        print("min2 is :" + str(min2))
-
+        print("min2 is:" + str(min2))
         minute_interval(min1, min2)
+    
+        min1 = 0
+        min2 = 0
+        print("restart min to :" + str(min2))
+        time.sleep(1)
+        
